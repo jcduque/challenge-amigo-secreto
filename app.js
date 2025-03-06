478d2d984bf47b53301d22301095d84cc0b139b6
Jean Carlos Duque <pipem14@gmail.com>
2025-03-06 17:09:34 -0500

    modifica y valida comentarios

--- app.js
+++ app.js
@@ -5,14 +5,12 @@ function agregarAmigo() {
     let inputAmigo = document.getElementById("amigo")
     let nombreAmigo = inputAmigo.value
     let validacion = false; // variable para saber si ya existe un nombre
-
     // validamos si el campo que espera el nombre se encuentra vacio
     if (nombreAmigo === "") {
         alert("Escriba un nombre válido");
         inputAmigo.focus();
         return;
     }
-
     // Validamos si el nombre ya existe en la lista
     for(let k = 0; k < amigos.length; k++){
             if(amigos[k] == nombreAmigo){
@@ -28,8 +26,7 @@ function agregarAmigo() {
         // agregamos el nombre al array amigos
         amigos.push(nombreAmigo);
         // llamamos a la funcion renderizarAmigos
-        renderizarAmigos();
-        
+        renderizarAmigos();       
         // Limpiamos el campo de entrada
         inputAmigo.value = "";
         inputAmigo.focus();
@@ -37,16 +34,14 @@ function agregarAmigo() {
 }
 
 function renderizarAmigos(){
-    
     // Limpiamos la lista existente antes de agregar los nuevos elementos
     let listaAmigos = document.getElementById("listaAmigos");
     listaAmigos.innerHTML = ""; // Limpiamos el contenido de la lista
-    
     // usamos el ciclo for sobre el arreglo amigos y agregamos cada nombre como un <li>
     for (let i = 0; i < amigos.length; i++) {
-        let li = document.createElement("li");
-        li.textContent = amigos[i]; // Asignamos el nombre de amigo al <li>
-        listaAmigos.appendChild(li); // Agregamos el <li> al contenedor de la lista
+        let lista = document.createElement("li");
+        lista.textContent = amigos[i]; // Asignamos el nombre de amigo al <li>
+        listaAmigos.appendChild(lista); // Agregamos el <li> al contenedor de la lista
     }
 }
 
@@ -57,14 +52,11 @@ function sortearAmigo() {
         alert("Necesitas añadir algunos nombres para poder realizar el sorteo");
         return; // Salir de la función si no hay amigos
     }
-
     // Escoge un amigo aleatorio
     let amigoElegido = amigos[Math.floor(Math.random() * amigos.length)];
-
     // Mostrar el resultado del sorteo
     const resultadoSorteo = document.querySelector("#resultado");
     resultadoSorteo.textContent = `El amigo sorteado es: ${amigoElegido}`;
-
     // Limpiar la lista de amigos mostrada en pantalla
     const listaAmigos = document.querySelector("#listaAmigos");
     listaAmigos.innerHTML = ""; // Limpiar la lista de amigos
